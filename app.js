4056099f430f8c20e81908cd66363f6d63666871
ELIUS <eliushossain67@gmail.com>
2021-08-20 19:27:23 +0600

    pin matched already working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// generate pin 
 function getPin() {
     const pin = Math.round(Math.random() * 10000)
     const stringPin = pin + ''
@@ -10,4 +11,35 @@ function getPin() {
 function genaratPin() {
     const pin = getPin()
     document.getElementById('generatInput').value = pin
+}
+
+// calculate pin
+function keyPad(event) {
+    const keyText = event.target.innerText
+    const inputValue = document.getElementById('inputValue')
+    if (isNaN(keyText)) {
+        if (keyText == 'C') {
+            inputValue.value = ''
+        }
+    } else {
+        const pinNumber = inputValue.value + keyText
+        inputValue.value = pinNumber
+    }
+}
+// pin number match
+function pinMatch() {
+    const genaratId = document.getElementById('generatInput')
+    const calPinId = document.getElementById('inputValue')
+    const genaratIdValue = genaratId.value
+    const calPinIdValue = calPinId.value
+    const errorNotify = document.getElementById('errorNotify')
+    const showNotify = document.getElementById('showNotify')
+    if (genaratIdValue == calPinIdValue) {
+        showNotify.style.display = 'block'
+        errorNotify.style.display = 'none'
+    } else {
+        errorNotify.style.display = 'block'
+        showNotify.style.display = 'none'
+    }
+
 }
